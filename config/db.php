92eb550ec2a38a4f3f3c3970b64685d86241bc9b
gjc <gjc@126.com>
2016-11-20 19:51:05 +0800

    sdfsdsdf

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: snow
+ * Date: 2016/11/20
+ * Time: 18:41
+ */
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=home',
+    'username' => 'home',
+    'password' => 'dl2983252',
+    'charset' => 'utf8',
+//    'tablePrefix'=>'yii_computer_',//not need tablePrefix
+];
